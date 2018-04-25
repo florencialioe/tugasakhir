5d343a3c4450cc2af3431f52594cfb117b251f34
florencia <littleflorencia@gmail.com>
2018-04-25 22:53:54 +0700

    link menukuis

---
+++ UnitySkripsi/Assets/menukuis.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menukuis : MonoBehaviour {
+    public void changemenuscene(string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+}
