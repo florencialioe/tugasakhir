168f5d643a53270996b975d955e1986fd6fb3cbe
florencia <littleflorencia@gmail.com>
2018-04-28 17:14:30 +0700

    Banyak Berubah

---
+++ UnitySkripsi/Assets/CreateHalBel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+
+public class halbel
+{
+    public bool ba;
+    public bool hom;
+    public bool st;
+    public bool belden;
+    public bool belmen;
+    public Sprite gmbrhwn;
+    public string han;
+    public string pin;
+    public string ar;
+    public Button rep;
+    public Button ne;
+    public Button kem;
+}
+public class CreateHalBel : MonoBehaviour {
+
+    Scene newScene = SceneManager.CreateScene("My New Scene");
+
+    public GameObject sampleisibel;
+    public Transform contentPanel;
+    public List<halbel> itemList;
+
+	
+}
---
+++ UnitySkripsi/Assets/Scripts/CreateScrollList.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[System.Serializable]
+
+public class item
+{
+    public string han;
+    public string pin;
+    public string ar;
+    public Sprite gambar;
+    public bool star;
+}
+
+public class CreateScrollList : MonoBehaviour {
+    public GameObject sampleButton;
+    public Transform contentPanel;
+    public List<item> itemList;
+
+    [SerializeField] ScrollRect scrollRect;
+    [SerializeField] RectTransform content_Panel;
+    [SerializeField] Vector2 valueX;
+
+    void Start()
+    {
+        PopulateList();
+        Snapto(valueX);
+    }
+
+    public void Snapto(Vector2 alue)
+    {
+       Canvas.ForceUpdateCanvases();
+       content_Panel.anchoredPosition = (Vector2)scrollRect.transform.InverseTransformPoint(alue);
+
+    }
+
+    private void Update()
+    {
+        //Snapto(valueX);
+    }
+    void PopulateList()
+    {
+        foreach (var item in itemList)
+        {
+            GameObject newButton = Instantiate(sampleButton) as GameObject;
+            SampleButtonScript buttonScript = newButton.GetComponent<SampleButtonScript>();
+            buttonScript.hanzi.text = item.han;
+            buttonScript.pinyin.text = item.pin;
+            buttonScript.arti.text = item.ar;
+            buttonScript.gambarhewan.sprite = item.gambar;
+            buttonScript.bintang.SetActive(item.star);
+
+            newButton.transform.SetParent(contentPanel);
+        }
+    }
+}
