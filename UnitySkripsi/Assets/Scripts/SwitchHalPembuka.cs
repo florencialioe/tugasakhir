ccf52758500f2885019b21340d168fa9195332e5
florencia <littleflorencia@gmail.com>
2018-05-01 00:44:37 +0700

    perubahan

---
+++ UnitySkripsi/Assets/Scripts/SwitchHalPembuka.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchHalPembuka : MonoBehaviour
+{
+    public float delay;
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay));
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("halamanutama");
+    }
+}
