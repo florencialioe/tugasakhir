f8eea90ef929691876fcd41d18e4c51aae75aea3
florencia <littleflorencia@gmail.com>
2018-05-05 01:58:49 +0700

    banyak perubahan

---
+++ UnitySkripsi/Assets/Scripts/coba.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class coba : MonoBehaviour {
+
+   
+    public Image gambarhewan;
+    public Text hanzi;
+    public Text pinyin;
+    public Text arti;
+    
+   
+}
